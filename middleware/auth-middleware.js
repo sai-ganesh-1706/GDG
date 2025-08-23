4898066d3a2216099e1244fbc921b5e2c3614fd7
sai-ganesh-1706 <saiganesh.b24@iiits.in>
2025-08-23 20:52:49 +0530

    3rd commit

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -3,7 +3,9 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token =
-     (authHeader && authHeader.split(" ")[1]);
+     req.cookies?.accessToken || (authHeader && authHeader.split(" ")[1]);
+
+ (authHeader && authHeader.split(" ")[1]);
 
   if (!token) {
     return res.status(401).json({
